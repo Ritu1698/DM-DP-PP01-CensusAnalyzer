f04c1b42e2c6800f4c41e1038832b934dbbf1668
Ritu1698 <biswasrituparna83@gmail.com>
2020-10-24 08:05:59 +0530

    [Rituparna]Added: Unit Test to Check Sorting Method And Output Of Sorted List Stored in File

--- src/test/java/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -1,6 +1,7 @@
 package censusanalyser;
 
 import com.bridgelabz.csvbuilder.CSVException;
+import com.google.gson.Gson;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -178,5 +179,21 @@ public class CensusAnalyserTest {
             e.printStackTrace();
             Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
         }
+    }
+
+    @Test
+    public void givenIndianCensusData_whenSortedOnState_shouldReturnSortedResult() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+            String sortedCensusData = censusAnalyser.getStateWiseSortedCensusData();
+            IndiaCensusCSV[] censusCSV = new Gson().fromJson(sortedCensusData, IndiaCensusCSV[].class);
+            int censusCSVLength = censusCSV.length;
+            Assert.assertEquals("Andhra Pradesh", censusCSV[0].state);
+            Assert.assertEquals("West Bengal", censusCSV[censusCSVLength - 1].state);
+        } catch (CensusAnalyserException e) {
+
+        }
+
     }
 }
